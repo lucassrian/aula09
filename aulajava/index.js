5232e095352a3093b73546ab9a2236f8cd736627
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-10 11:56:30 -0300

    string

--- aulajava/index.js
+++ aulajava/index.js
@@ -2,3 +2,7 @@ console.log("Hellow World")
 const nome = "Lucas"
 const idade = 16
 console.log("meu nome é ", nome ," e tenho ", idade ," anos")
+const idadeNumero = 16
+const idadeTexto = idadeNumero.toString()
+console.log(typeof idadeNumero)
+console.log(typeof idadeTexto)
