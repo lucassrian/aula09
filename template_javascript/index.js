73f436968b615440b829880dd2a4533cbf2207bf
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-11 10:44:44 -0300

    git-1

---
+++ template_javascript/index.js
@@ -0,0 +1,7 @@
+const primeiroNome = "Lucas"
+const segundoNome = "Sousa"
+const idadeNumero = 16
+const serEstudante = "Sim"
+let nome = prompt("Qual é seu nome?")
+let sobrenome = prompt("Qual é seu sobrenome?")
+alert(`Seu nome é ${nome} seu sobrenome ${sobrenme}`)
