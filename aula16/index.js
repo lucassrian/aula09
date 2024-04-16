4aa11b6b1887a645ca1f2595e0180560dfd2c8c7
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-16 11:53:25 -0300

    surtando um pouquinho

--- aula16/index.js
+++ aula16/index.js
@@ -1,4 +1,4 @@
-let primeiroNumero
+/*let primeiroNumero
 let segundoNumero
 let resultado
 
@@ -17,7 +17,57 @@ resultado = primeiroNumero < segundoNumero
 alert(resultado)
 
 resultado = primeiroNumero > segundoNumero
+alert(resultado)*/
+
+//Declaração de variavel 
+/*let a
+let b 
+let c 
+let resultado
+
+//Leitura de dados informados oelo usuario
+a = true
+b = false 
+c = true
+
+//Teste de comparação
+resultado = a && b
 alert(resultado)
 
-//Mostra resultado na tela
+resultado = b && c 
 alert(resultado)
+
+resultado = a && b && c
+alert(resultado)
+
+resultado = a || b 
+alert(resultado)
+
+resultado = b || c
+alert(resultado)
+
+resultado = a || c
+alert(resultado)
+
+resutado = a || b || c 
+alert(resultado)*/
+
+let nome 
+let anoNascimento 
+let anoAtual
+let idade
+let maiorIdade
+let idade2050
+
+nome = prompt("Digite seu nome:")
+anoNascimento = prompt("Digite o ano que você nasceu:")
+anoAtual = prompt("Digite o ano atual:")
+
+idade = anoAtual - anoNascimento
+maiorIdade = idade >= 18
+idade2050 = 2050 - anoNascimento
+
+alert(nome)
+alert(idade)
+alert(maiorIdade)
+alert(idade2050)
