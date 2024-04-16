45d9b45acb94027e394f6d0eb3bb9d01fc188114
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-16 10:16:26 -0300

    true or false?

---
+++ aula16/index.js
@@ -0,0 +1,23 @@
+let primeiroNumero
+let segundoNumero
+let resultado
+
+//leitura de dados informados pelo usuario
+primeiroNumero = prompt("Digite o primeiro número")
+segundoNumero = prompt("Digite o segundo número") 
+
+//Teste de comparação
+resultado = primeiroNumero === segundoNumero
+alert(resultado)
+
+resultado = primeiroNumero !== segundoNumero
+alert(resultado)
+
+resultado = primeiroNumero < segundoNumero
+alert(resultado)
+
+resultado = primeiroNumero > segundoNumero
+alert(resultado)
+
+//Mostra resultado na tela
+alert(resultado)
