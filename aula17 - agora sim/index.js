7607d85e5f4692784315b9ef3de56d39745256dc
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-17 11:19:53 -0300

    Ativ. 4

---
+++ aula17 - agora sim/index.js
@@ -0,0 +1,23 @@
+/*let idade 
+let idadeAmigo
+let diferençaIdade
+
+idade = prompt("Qual é a sua idade?")
+idadeAmigo = prompt("Qual é a idade do seu amigo?")
+
+resultado = idade >= idadeAmigo
+alert(`Sua idade é maior que a do seu amigo? ${resultado}`)
+
+diferençaIdade = idade - iadadeAmigo
+alert(`A diferença de idade é ${diferençaIdade}`)*/
+
+/*let numeroUm
+
+numeroUm = prompt("Digite um numero par:")
+console.log((numeroUm % 2) == 0)*/
+
+let idadeAnos
+idadeAnos = Number(prompt("Quantos anos você tem?"))
+alert(idadeAnos *12)
+alert(idadeAnos * 360)
+alert(idadeAnos * 8786)
