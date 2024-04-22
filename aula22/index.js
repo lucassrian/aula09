fea841071adee796efe8ee883d97611e40284aa8
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-22 11:04:42 -0300

    array

---
+++ aula22/index.js
@@ -0,0 +1,7 @@
+
+const listaComida = ["Churrasco", "Sorvete", "Pastel", "Lasanha", "Bolo"]
+console.log(listaComida)
+console.log("Essas são as minhas comidas peferidas:" + "\n" + listaComida[0] + "\n" + listaComida[1]+ "\n" + listaComida[2]+ "\n" + listaComida[3]+ "\n" + listaComida[4])
+
+const comida1 = prompt("E qual é a sua comida favorita?")
+alert("Essas são as minhas comidas peferidas:" + "\n" + comida1 + "\n" + listaComida[1]+ "\n" + listaComida[2]+ "\n" + listaComida[3]+ "\n" + listaComida[4])
