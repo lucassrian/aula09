b865a584d650e66b40db64f72a608664b1134de3
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-06-07 11:23:52 -0300

    trabalho final

--- trabalho-final/index.js
+++ trabalho-final/index.js
@@ -1,3 +1,6 @@
+
+let usuario = []
+
 function imprimirMenu(){
     let mensagem = ""
      mensagem+= "//// Sistema De Cadastro De Doadores De Sangue //// \n"
@@ -11,7 +14,41 @@ function imprimirMenu(){
 
     prompt(mensagem)
 
-     return opcao 
+    
+}
+imprimirMenu()
+
+function primeiraOpcao(){
+    let nome = prompt("Digite o seu nome:")
+    let idade = prompt("digite a sua idade:")
+    let peso = prompt("Digete o seu peso:")
+    let sangue = prompt("Digete o seu tipo sanguíneo:")
+    let data = prompt("Digite a data da sua última doação:")
+     usurio.push({ nome,idade,peso,sangue,data } )
+     alert("Parabéns, você está cadastrado!! ")
+     let = ""
+     mensagem += "///// Listagem de Doarores"
+
 }
 
-imprimirMenu()
+function segundaOpcao(){
+    let = ""
+     mensagem += "///// Listagem de Doarores /////"
+    prompt(mensagem)
+}
+
+
+
+
+function main{
+    swith(listarDoadores){
+        case 1:
+            primeiraOpcao()
+            break
+        case 2:
+            segundaOpcao()
+            break
+    }
+}
+
+main()
